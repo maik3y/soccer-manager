ff59db0d336e9d09f836009e79b17605786f8839
Maikey Sip <AGM.sip@student.han.nl>
2019-01-20 12:15:23 +0100

    added new styles to entire app

--- server.js
+++ server.js
@@ -3,15 +3,20 @@ const fetch = require('node-fetch');
 const app = express();
 
 app.use(express.static('.'));
-app.get = function (s, f) {};
 app.get('/player/:name', (req, res) => {
 
-    fetch('https://www.easports.com/fifa/ultimate-team/api/fut/item?name=' + req.params.name)
-        .then( (res) => { return res.json() } )
-        .then( (data) => {
-            res.header('Access-Control-Allow-Origin','*');
-            res.end(JSON.stringify(data));
-        });
+    if (req.params.name.length > 0) {
+        try {
+            fetch('https://www.easports.com/fifa/ultimate-team/api/fut/item?name=' + req.params.name)
+                .then((res) => {
+                    return res.json()
+                })
+                .then((data) => {
+                    res.header('Access-Control-Allow-Origin', '*');
+                    res.end(JSON.stringify(data));
+                });
+        } catch (e) { }
+    }
 
 });
 
---
+++ src/Sidebar/Sidebar.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Sidebar extends React.Component {
+    render() {
+        return (
+            <div className="sidebar">
+                <div className="sidebar__header">
+                    <h1 className="sidebar__title">Squad builder</h1>
+                </div>
+                <div className="sidebar__content">
+                    <div className="rating">
+                        <h2 className="rating__title">Overall rating</h2>
+                        <span className="rating__total">{this.props.rating}</span>
+                    </div>
+                </div>
+                <div className="sidebar__footer">
+                    <div className="btn btn--red btn--big"
+                         onClick={this.props.clear()}>Clear formation
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Sidebar;
